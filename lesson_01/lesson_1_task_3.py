995aebdf70516db0ad6b965a379253cdb58c6e33
Пользователь <misha.vania2019@gmail.com>
2026-04-01 12:03:36 +0300

    Your commit message

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -3,4 +3,4 @@ last_name = ''
 first_name = input('Введите имя:')
 last_name = input('Введите фамилию:')
 
-print('Вас зовут:' + last_name + '' + first_name)
+print('Вас зовут:' + last_name + ' ' + first_name)
